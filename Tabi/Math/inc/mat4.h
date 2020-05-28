f761e4f959b87f0c8e1d2bd9fde71664df81c73c
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-05-28 22:31:36 +0200

    Added translation and scaling for mat4

--- Tabi/Math/inc/mat4.h
+++ Tabi/Math/inc/mat4.h
@@ -21,9 +21,24 @@ namespace tabi
             mat4 Transpose() const;
             static mat4 Identity();
 
+            void Scale(const float a_Scale);
+            void Scale(const vec3& a_Scale);
+            void Scale(const vec4& a_Scale);
+            static mat4 Scale(const mat4& a_Matrix, const float a_Scale);
+            static mat4 Scale(const mat4& a_Matrix, const vec3& a_Scale);
+            static mat4 Scale(const mat4& a_Matrix, const vec4& a_Scale);
+
+            void Translate(const vec3& a_Translation);
+            void Translate(const vec4& a_Translation);
+            static mat4 Translate(const mat4& a_Matrix, const vec3& a_Translation);
+            static mat4 Translate(const mat4& a_Matrix, const vec4& a_Translation);
+
+            // TODO: Rotation
+
             float Get(const unsigned int a_Row, const unsigned int a_Column) const;
             float& Get(const unsigned int a_Row, const unsigned int a_Column);
-
+            
+            /**** Operators ****/
             float operator[](const unsigned int a_Index) const;
             float& operator[](const unsigned int a_Index);
             
--- Tabi/Math/src/mat4.cpp
+++ Tabi/Math/src/mat4.cpp
@@ -25,12 +25,103 @@ mat4 mat4::Identity()
 {
     mat4 identity;
 
-    identity.Get(0, 0) = 1.0f;
-    identity.Get(1, 1) = 1.0f;
-    identity.Get(2, 2) = 1.0f;
-    identity.Get(3, 3) = 1.0f;
+    identity.m[0][0] = 1.0f;
+    identity.m[1][1] = 1.0f;
+    identity.m[2][2] = 1.0f;
+    identity.m[3][3] = 1.0f;
 
     return identity;
+}
+
+void tabi::math::mat4::Scale(const float a_Scale)
+{
+    m[0][0] *= a_Scale;
+    m[1][1] *= a_Scale;
+    m[2][2] *= a_Scale;
+}
+
+void tabi::math::mat4::Scale(const vec3& a_Scale)
+{
+    m[0][0] *= a_Scale.x;
+    m[1][1] *= a_Scale.y;
+    m[2][2] *= a_Scale.z;
+}
+
+void tabi::math::mat4::Scale(const vec4& a_Scale)
+{
+    m[0][0] *= a_Scale.x;
+    m[1][1] *= a_Scale.y;
+    m[2][2] *= a_Scale.z;
+    m[3][3] *= a_Scale.w;
+}
+
+mat4 tabi::math::mat4::Scale(const mat4& a_Matrix, const float a_Scale)
+{
+    mat4 result = a_Matrix;
+
+    result.m[0][0] *= a_Scale;
+    result.m[1][1] *= a_Scale;
+    result.m[2][2] *= a_Scale;
+
+    return result;
+}
+
+mat4 tabi::math::mat4::Scale(const mat4& a_Matrix, const vec3& a_Scale)
+{
+    mat4 result = a_Matrix;
+
+    result.m[0][0] *= a_Scale.x;
+    result.m[1][1] *= a_Scale.y;
+    result.m[2][2] *= a_Scale.z;
+
+    return result;
+}
+
+mat4 tabi::math::mat4::Scale(const mat4& a_Matrix, const vec4& a_Scale)
+{
+    mat4 result = a_Matrix;
+
+    result.m[0][0] *= a_Scale.x;
+    result.m[1][1] *= a_Scale.y;
+    result.m[2][2] *= a_Scale.z;
+    result.m[3][3] *= a_Scale.w;
+
+
+    return result;
+}
+
+void tabi::math::mat4::Translate(const vec3& a_Translation)
+{
+    m[0][3] += a_Translation.x;
+    m[0][3] += a_Translation.y;
+    m[0][3] += a_Translation.z;
+}
+
+void tabi::math::mat4::Translate(const vec4& a_Translation)
+{
+    assert(a_Translation.w != 0.0f);
+    m[0][3] += a_Translation.x / a_Translation.w;
+    m[0][3] += a_Translation.y / a_Translation.w;
+    m[0][3] += a_Translation.z / a_Translation.w;
+}
+
+mat4 tabi::math::mat4::Translate(const mat4& a_Matrix, const vec3& a_Translation)
+{
+    mat4 result = a_Matrix;
+    
+    result.Translate(a_Translation);
+
+    return result;
+}
+
+mat4 tabi::math::mat4::Translate(const mat4& a_Matrix, const vec4& a_Translation)
+{
+    assert(a_Translation.w != 0);
+    mat4 result = a_Matrix;
+
+    result.Translate(a_Translation);
+
+    return result;
 }
 
 float& tabi::math::mat4::Get(const unsigned int a_Row, const unsigned int a_Column)
@@ -113,7 +204,7 @@ mat4 tabi::math::operator*(const mat4& a_Lhs, const float a_Scalar)
 
 vec3 tabi::math::operator*(const mat4& a_Lhs, const vec3& a_Rhs)
 {
-    vec4 result = a_Lhs * vec4(a_Rhs, 0.0f);
+    vec4 result = a_Lhs * vec4(a_Rhs, 1.0f);
     return vec3(result.x, result.y, result.z);
 }
 
@@ -125,7 +216,7 @@ vec4 tabi::math::operator*(const mat4& a_Lhs, const vec4& a_Rhs)
     {
         for (auto j = 0; j < 4; ++j)
         {
-            result[i] += a_Lhs.Get(j, i) * a_Rhs[i];
+            result[i] += a_Lhs.m[j][i] * a_Rhs[i];
         }
     }
     
@@ -140,11 +231,11 @@ mat4 tabi::math::operator*(const mat4& a_Lhs, const mat4& a_Rhs)
     {
         for (auto j = 0; j < 4; ++j)
         {
-            result.Get(i, j) = 0.0f;
+            result.m[i][j] = 0.0f;
 
             for (auto k = 0; k < 4; ++k)
             {
-                result.Get(i, j) += a_Lhs.Get(i, k) * a_Rhs.Get(k, j);
+                result.m[i][j] += a_Lhs.m[i][k] * a_Rhs.m[k][j];
             }
         }
     }

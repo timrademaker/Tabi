772a8e0b891e4904417e3dbb8ae402be8c364e68
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-06-20 17:41:31 +0200

    Added Update, BindButton and BindAxis to IInputHandler

--- Tabi/Input/inc/IInputHandler.h
+++ Tabi/Input/inc/IInputHandler.h
@@ -8,6 +8,10 @@ namespace tabi
         static IInputHandler& GetInstance();
 
         // No virtual function to handle messages - this could differ per platform. Application should deal with making sure the correct function is called, with the correct arguments.
+        virtual void Update() = 0;
+
+        virtual void BindButton(unsigned int a_Button) = 0;
+        virtual void BindAxis(unsigned int a_Axis) = 0;
 
         virtual bool IsButtonDown(unsigned int a_Button) = 0;
 

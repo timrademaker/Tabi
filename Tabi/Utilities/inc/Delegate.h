77a28de4b99712965087c3ee1364bb14cc44e860
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-08-09 23:10:38 +0200

    Unsubscribing now works
    (Now storing a pointer to the subscribed object as well)

--- Tabi/Utilities/inc/Delegate.h
+++ Tabi/Utilities/inc/Delegate.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <functional>
-
 #include <TabiTypes.h>
+
+#include <functional>
 
 #define DECLARE_DELEGATE(DelegateName, ...) \
     using DelegateName = tabi::DelegateBase<__VA_ARGS__>;
@@ -12,41 +12,47 @@ namespace tabi
     template<typename... DelegateArgs>
     class DelegateBase
     {
+        using UserClass = void;
+
         using DelegateType = std::function<void(DelegateArgs...)>;
+        using DelegateMap = tabi::unordered_map<UserClass*, tabi::vector<DelegateType>>;
     public:
         DelegateBase() = default;
         ~DelegateBase() = default;
 
-        void Add(DelegateType a_Func);
-        void Remove(DelegateType a_Func);
+        void Add(UserClass* a_Object, DelegateType a_Func);
+        void Remove(UserClass* a_Object);
         void RemoveAll();
 
         void Broadcast(DelegateArgs... a_Args);
 
     private:
-        // Alternatively, make this a multimap/set so that an entire object can be unsubbed at once (and to make Remove easier to make).
-        tabi::vector<DelegateType> m_Delegates;
+        DelegateMap m_Delegates;
 
     };
 
 
     template<typename ...DelegateArgs>
-    inline void DelegateBase<DelegateArgs...>::Add(DelegateType a_Func)
+    inline void DelegateBase<DelegateArgs...>::Add(UserClass* a_Object, DelegateType a_Func)
     {
-        m_Delegates.push_back(a_Func);
+        m_Delegates[a_Object].push_back(a_Func);
     }
 
+
     template<typename ...DelegateArgs>
-    inline void DelegateBase<DelegateArgs...>::Remove(DelegateType a_Func)
+    inline void DelegateBase<DelegateArgs...>::Remove(UserClass* a_Object)
     {
-        for (std::size_t i = 0; i < m_Delegates.size(); ++i)
+        auto iter = m_Delegates.find(a_Object);
+        if (iter != m_Delegates.end())
         {
-            //if (m_Delegates[i] == a_Func)
-            //{
-            //    m_Delegates.erase(m_Delegates.begin() + i);
-            //    return;
-            //}
+            m_Delegates.erase(iter);
         }
+#if defined(_DEBUG)
+        else
+        {
+            tabi::logger::TabiWarn("Tried to unbind a delegate, but nothing was bound!");
+        }
+#endif
     }
 
     template<typename ...DelegateArgs>
@@ -58,9 +64,12 @@ namespace tabi
     template<typename ...DelegateArgs>
     inline void DelegateBase<DelegateArgs...>::Broadcast(DelegateArgs ...a_Args)
     {
-        for (DelegateType& d : m_Delegates)
+        for (auto iter = m_Delegates.begin(); iter != m_Delegates.end(); ++iter)
         {
-            d(a_Args...);
+            for (DelegateType& d : iter->second)
+            {
+                d(a_Args...);
+            }
         }
     }
 }

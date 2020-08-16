5c5c994a71817ec4db0493f4ccbd1c0f529a427d
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-08-16 17:51:17 +0200

    Renamed Subscribe_OneParam macro to Subscribe, as this is likely used more often
    Also renamed the class to EventBase rather than DelegateBase, as the class sends out events to delegates, and is not a delegate itself.

--- Tabi/Utilities/inc/Delegate.h
+++ Tabi/Utilities/inc/Delegate.h
@@ -4,32 +4,33 @@
 
 #include <functional>
 
-#define DECLARE_DELEGATE(DelegateName, EventClass) \
-    using DelegateName = tabi::DelegateBase<EventClass>
+#define DECLARE_EVENT(EventName, EventClass) \
+    using EventName = tabi::EventBase<EventClass>
 
 /**
 * @brief Helper macros to subscribe to events
 * @params Subscriber The object subscribing to the event
 * @params Callback The callback function to send events to
 */
-#define Subscribe(Subscriber, Callback)             Add(&Subscriber, std::bind(&Callback, Subscriber))
-#define Subscribe_OneParam(Subscriber, Callback)    Add(&Subscriber, std::bind(&Callback, Subscriber, std::placeholders::_1))
+#define Subscribe_NoParams(Subscriber, Callback)    Add(&Subscriber, std::bind(&Callback, Subscriber))
+#define Subscribe(Subscriber, Callback)             Add(&Subscriber, std::bind(&Callback, Subscriber, std::placeholders::_1))
 
 namespace tabi
 {
     template<typename EventInfo>
-    class DelegateBase
+    class EventBase
     {
         using UserClass = void;
 
         using DelegateType = std::function<void(EventInfo)>;
         using DelegateMap = tabi::unordered_map<UserClass*, tabi::vector<DelegateType>>;
     public:
-        DelegateBase() = default;
-        ~DelegateBase() = default;
+        EventBase() = default;
+        ~EventBase() = default;
 
         /**
-        * @brief Subscribe an object to an event. Note: It is easier to use the Subscribe-macro rather than this function directly
+        * Subscribe an object to an event. You can use the Add-function together with the TABI_BIND-macro (Add(&object, TABI_BIND_OneParam(object, Class::Function))), or the Subscribe-macro (Subscribe(object, Class::Function))
+        * @brief Subscribe an object to an event.
         * @params a_Object The object subscribing to the event
         * @params a_Callback The callback function to send events to
         */
@@ -54,6 +55,7 @@ namespace tabi
         void RemoveAll();
 
         /**
+        * Broadcasts an event to all subscribers. If ReturnType is bool, a return value of "true", it means the event has been consumed and should not be sent out to other delegates.
         * @brief Broadcasts an event to all subscribers
         * @params a_Event The message sent to all subscribers' callback functions
         */
@@ -66,7 +68,7 @@ namespace tabi
 
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::Add(UserClass* a_Object, DelegateType a_Callback)
+    inline void EventBase<EventInfo>::Add(UserClass* a_Object, DelegateType a_Callback)
     {
         if (a_Callback)
         {
@@ -82,14 +84,14 @@ namespace tabi
     }
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::AddStatic(DelegateType a_Callback)
+    inline void EventBase<EventInfo>::AddStatic(DelegateType a_Callback)
     {
         return Add(nullptr, a_Callback);
     }
 
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::Remove(UserClass* a_Object)
+    inline void EventBase<EventInfo>::Remove(UserClass* a_Object)
     {
         auto iter = m_Delegates.find(a_Object);
         if (iter != m_Delegates.end())
@@ -105,19 +107,19 @@ namespace tabi
     }
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::RemoveStatic()
+    inline void EventBase<EventInfo>::RemoveStatic()
     {
         return Remove(nullptr);
     }
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::RemoveAll()
+    inline void EventBase<EventInfo>::RemoveAll()
     {
         m_Delegates.clear();
     }
 
     template<typename EventInfo>
-    inline void DelegateBase<EventInfo>::Broadcast(EventInfo a_Event)
+    inline void EventBase<EventInfo>::Broadcast(EventInfo a_Event)
     {
         for (auto iter = m_Delegates.begin(); iter != m_Delegates.end(); ++iter)
         {
@@ -129,5 +131,5 @@ namespace tabi
     }
 
     struct EmptyEvent {};
-    using Delegate = tabi::DelegateBase<EmptyEvent>;    
+    using Delegate = tabi::EventBase<EmptyEvent>;
 }

349720dd918060c985466b1bf551b4d6ecb54885
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-09-20 18:31:12 +0200

    Added doxygen documentation

--- Tabi/ECS/inc/IComponentArray.h
+++ Tabi/ECS/inc/IComponentArray.h
@@ -7,8 +7,14 @@ namespace tabi
     class IComponentArray
     {
     public:
+        IComponentArray() = default;
+        IComponentArray(IComponentArray&) = delete;
+        IComponentArray(const IComponentArray&) = delete;
         virtual ~IComponentArray() = default;
 
+        /**
+        * @brief Called when an entity is destroyed, so that it can be removed from the component array
+        */
         virtual void OnEntityDestroyed(const Entity::ID_t a_EntityID) = 0;
     };
 }

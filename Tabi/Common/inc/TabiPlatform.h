2c59f84febd59602a24813c242f5f1ff156119fa
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-10-29 20:43:31 +0100

    Added TabiPlatform, which includes platform-specific headers
    Not quite related to this branch, but it's related to the next commit

---
+++ Tabi/Common/inc/TabiPlatform.h
@@ -0,0 +1,5 @@
+#if defined(_WINDOWS)
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
--- Tabi/Common/inc/TabiTypes.h
+++ Tabi/Common/inc/TabiTypes.h
@@ -3,11 +3,7 @@
 #include <TabiContainers.h>
 #include <TabiPointers.h>
 
-#if defined(_WINDOWS)
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#endif
+#include <TabiPlatform.h>
 
 namespace tabi
 {

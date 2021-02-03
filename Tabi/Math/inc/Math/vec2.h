c37252b9ea2bc31a19b5de55856c24004c992d9f
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-02-03 15:50:01 +0100

    Fixed issues with vec2

--- Tabi/Math/inc/Math/vec2.h
+++ Tabi/Math/inc/Math/vec2.h
@@ -13,14 +13,24 @@ namespace tabi
         vec2() : x(0), y(0) {}
         vec2(float a_X, float a_Y) : x(a_X), y(a_Y) {}
 
+        float Dot(const vec2& a_Other) const;
+        void Normalize();
+
+        static float Dot(const vec2& a_Vec1, const vec2& a_Vec2);
+        static vec2 Normalize(const vec2& a_Vector);
+
+        float Length() const;
+        float LengthSquared() const;
+
+
         /**** Operators ****/
         float& operator[](const unsigned int a_Index);
         float operator[](const unsigned int a_Index) const;
 
         vec2& operator=(const vec2& a_Rhs);
 
-        bool operator==(const vec2& a_Rhs);
-        bool operator!=(const vec2& a_Rhs);
+        bool operator==(const vec2& a_Rhs) const;
+        bool operator!=(const vec2& a_Rhs) const;
 
         vec2& operator+=(const vec2& a_Rhs);
         vec2& operator+=(const float a_Rhs);
@@ -39,7 +49,7 @@ namespace tabi
 
         friend vec2 operator-(const vec2& a_Lhs, const vec2& a_Rhs);
         friend vec2 operator-(const vec2& a_Lhs, const float a_Rhs);
-        vec2 operator-();
+        vec2 operator-() const;
 
         friend vec2 operator*(const vec2& a_Lhs, const vec2& a_Rhs);
         friend vec2 operator*(const vec2& a_Lhs, const float a_Rhs);

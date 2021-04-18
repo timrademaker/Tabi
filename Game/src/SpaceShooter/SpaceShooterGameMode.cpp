82609c3cd07224f41526018b85ac0302dbe78655
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-04-18 19:01:09 +0200

    Update SpaceShooterGameMode.cpp

--- Game/src/SpaceShooter/SpaceShooterGameMode.cpp
+++ Game/src/SpaceShooter/SpaceShooterGameMode.cpp
@@ -1,12 +1,15 @@
 #include "SpaceShooter/SpaceShooterGameMode.h"
 
 #include "Systems/MovementSystem.h"
+
+#include "Components/MovementComponent.h"
 #include <Components/Transform.h>
 
 
 bool SpaceShooter::SpaceShooterGameMode::OnInitialize()
 {
     m_ECS.RegisterSystem<SpaceShooter::MovementSystem>();
+    m_ECS.SetComponentTypeRequired<SpaceShooter::MovementSystem, SpaceShooter::MovementComponent>(true);
     m_ECS.SetComponentTypeRequired<SpaceShooter::MovementSystem, tabi::Transform>(true);
 
     return true;

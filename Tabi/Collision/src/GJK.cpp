1cdc06a6c4ce9a6c535da6537b0ae1c65ed36ebf
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-04-25 14:04:52 +0200

    Added early out when the origin of two colliders overlap

--- Tabi/Collision/src/GJK.cpp
+++ Tabi/Collision/src/GJK.cpp
@@ -6,9 +6,19 @@
 
 bool tabi::GJK::CollidersAreOverlapping(const ICollider* a_Collider1, const ICollider* a_Collider2)
 {
+    vec3 direction = a_Collider1->GetPosition() - a_Collider2->GetPosition();
+
+    if (direction.LengthSquared() == 0)
+    {
+        // The colliders are placed on top of each other
+        return true;
+        
+        // I can't think of any issues with this now (unless the origin of a collider can be outside of the collider itself),
+        // but this return might have to be removed and an arbitrary direction will have to be used instead if it turns out this causes issues
+    }
+
     ThreeSimplex simplex;
 
-    vec3 direction = a_Collider1->GetPosition() - a_Collider2->GetPosition();
     vec3 pointA = Support(a_Collider1, a_Collider2, direction);
     simplex.AddPoint(pointA);
 
@@ -23,7 +33,7 @@ bool tabi::GJK::CollidersAreOverlapping(const ICollider* a_Collider1, const ICol
         // Check if the point is past the origin
         if (newPoint.Dot(direction) < 0)
         {
-            // No intersection possible as the origin falls outside of the Minkowski shape
+            // No intersection possible as the origin falls outside of the Minkowski sum
             return false;
         }
 
@@ -44,6 +54,5 @@ tabi::vec3 tabi::GJK::Support(const ICollider* a_Collider1, const ICollider* a_C
     const vec3 pointA = a_Collider1->GetFurthestPointInDirection(a_Direction);
     const vec3 pointB = a_Collider2->GetFurthestPointInDirection(-a_Direction);
 
-    // TODO: Is this the correct order?
-    return pointB - pointA;
+    return pointA - pointB;
 }

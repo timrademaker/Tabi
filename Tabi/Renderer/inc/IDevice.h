a821bb70052237801e8b734378d22dfca69ffae0
timrademaker <10331650+timrademaker@users.noreply.github.com>
2022-07-17 22:47:56 +0200

    Added more documentation to IDevice class

--- Tabi/Renderer/inc/IDevice.h
+++ Tabi/Renderer/inc/IDevice.h
@@ -11,28 +11,69 @@ namespace tabi
 		virtual IDevice() = default;
 
 	public:
+		/**
+		 * @brief Initialize the rendering device
+		 */
 		virtual void Initialize() = 0;
+		/**
+		 * @brief Finalize the rendering device
+		 */
 		virtual void Finalize() = 0;
 
-		// NOTE: D3D12 uploads using a command list, but maybe just give the device a separate command list that uploads textures (and is executed at the end of a frame if needed)?
-		virtual class ITexture* CreateTexture(const struct TextureDescription&, const char* a_Data, size_t a_DataSize) = 0;
-		virtual class IBuffer* CreateBuffer(const struct BufferDescription&) = 0;
-		virtual class IShader* CreateShader(const struct ShaderDescription&) = 0;
-		virtual class ISampler* CreateSampler(const struct SamplerDescription&) = 0;
+		/**
+		 * @brief Create a new texture
+		 * @param a_TextureDescription A struct describing the texture to create
+		 * @returns The created texture
+		 */
+		virtual class ITexture* CreateTexture(const struct TextureDescription& a_TextureDescription) = 0;
+		/**
+		 * @brief Create a new buffer
+		 * @param a_BufferDescription A struct describing the buffer to create
+		 * @returns The created buffer
+		 */
+		virtual class IBuffer* CreateBuffer(const struct BufferDescription& a_BufferDescription) = 0;
+		/**
+		 * @brief Create a new shader
+		 * @param a_ShaderDescription A struct describing the shader to create
+		 * @returns The created shader
+		 */
+		virtual class IShader* CreateShader(const struct ShaderDescription& a_ShaderDescription) = 0;
+		/**
+		 * @brief Create a new sampler
+		 * @param a_SamplerDescription A struct describing the sampler to create
+		 * @returns The created sampler
+		 */
+		virtual class ISampler* CreateSampler(const struct SamplerDescription& a_SamplerDescription) = 0;
 
 		/**
 		 * @brief Create a new graphics pipeline object
 		 * @param a_PipelineDescription A struct describing the graphics pipeline to create
+		 * @returns The created graphics pipeline
 		 */
 		virtual class IGraphicsPipeline* CreateGraphicsPipeline(const struct GraphicePipelineDescription& a_PipelineDescription) = 0;
 		/**
 		 * @brief Create a new compute pipeline object
 		 * @param a_ComputePipelineDescription A struct describing the compute pipeline to create
+		 * @returns The created compute pipeline
 		 */
 		virtual class IComputePipeline* CreateComputePipeline(const struct ComputePipelineDescription& a_ComputePipelineDescription) = 0;
 
-		virtual class IRenderTarget* CreateRenderTarget(const struct RenderTargetDescription&) = 0;	// TODO: Some args (or a description) with things like format and size(?)
+		/**
+		 * @brief Create a new render target
+		 * @param a_RenderTargetDescription A struct describing the render target to create
+		 * @returns The created render target
+		 */
+		virtual class IRenderTarget* CreateRenderTarget(const struct RenderTargetDescription& a_RenderTargetDescription) = 0;
+		/**
+		 * @brief Create a new command list
+		 * @returns The created command list
+		 */
 		virtual class ICommandList* CreateCommandList() = 0;
+		/**
+		 * @brief Execute a command list
+		 * @param a_CommandList The command list to execute
+		 */
+		virtual void ExecuteCommandList(class ICommandList* a_CommandList) = 0;
 
 		virtual void DestroyTexture(class ITexture* a_Texture) = 0;
 		virtual void DestroyBuffer(class IBuffer* a_Buffer) = 0;
@@ -42,8 +83,6 @@ namespace tabi
 		virtual void DestroyComputePipeline(class IComputePipeline* a_ComputePipeline) = 0;
 		virtual void DestroyRenderTarget(class IRenderTarget* a_RenderTarget) = 0;
 		virtual void DestroyCommandList(class ICommandList* a_CommandList) = 0;
-
-		virtual void ExecuteCommandList(class ICommandList*) = 0;
 
 		/**
 		 * @brief Create a new fence object
@@ -57,9 +96,17 @@ namespace tabi
 		 */
 		virtual void InsertFence(class IFence* a_Fence, uint64_t a_Value) = 0;
 
-
+		/**
+		 * @brief Start a new frame
+		 */
 		virtual void BeginFrame() = 0;
+		/**
+		 * @brief End the current frame
+		 */
 		virtual void EndFrame() = 0;
+		/**
+		 * @brief Present the backbuffer to the screen
+		 */
 		virtual void Present() = 0;
 	};
 }

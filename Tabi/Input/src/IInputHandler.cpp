ae51e89ea9aaf1b910ffc3e998a314500da8efe9
timrademaker <10331650+timrademaker@users.noreply.github.com>
2022-10-30 13:20:05 +0100

    Fix CMake configuration for when Tabi is used as submodule

--- Tabi/Input/src/IInputHandler.cpp
+++ Tabi/Input/src/IInputHandler.cpp
@@ -2,6 +2,8 @@
 
 #if defined(_WINDOWS)
 #include "Windows/WindowsInputHandler.h"
+#else
+#error "No input method found for the current platform"
 #endif
 
 tabi::IInputHandler* tabi::IInputHandler::CreateInstance()
